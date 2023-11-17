fc29f719eef487fd31659abb6a909ef548254c8e
Augusto Cedro <augustocedro9@gmail.com>
2023-11-17 11:29:51 -0300

    "Primerio Commit JUNIT5"

---
+++ src/main/java/br/com/infnet/Util/CalculadoraUtil.java
@@ -0,0 +1,8 @@
+package br.com.infnet.Util;
+
+
+public class CalculadoraUtil {
+    public int dividir(int numero1, int numero2){
+            return numero1 / numero2;
+    }
+}
---
+++ src/test/java/br/com/infnet/calculadoraTeste.java
@@ -0,0 +1,27 @@
+package br.com.infnet;
+
+import br.com.infnet.Util.CalculadoraUtil;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+
+import static org.junit.jupiter.api.Assertions.*;
+
+
+public class calculadoraTeste {
+    @Test
+    @DisplayName("Deve retornar 1")
+    public void Calculadora(){
+        int numero1 = 4;
+        int numero2 = 3;
+        assertEquals(1, numero1 - numero2);
+    }
+    @Test
+    @DisplayName("Deve retornar um Erro no Calculo")
+    public void CalculadoraErro() {
+        CalculadoraUtil calculadoraUtil = new CalculadoraUtil();
+        assertThrows(ArithmeticException.class, () -> {
+            calculadoraUtil.dividir(4, 0);
+        });
+    }
+}
